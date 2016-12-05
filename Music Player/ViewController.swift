f7ca8476f4e3beb1c1abe7a5eff33975964484d9
Yawo Echitey <Yawo@yawomac.local>
2016-12-05 13:58:25 -0200

    Initial Commit

--- Music Player/ViewController.swift
+++ Music Player/ViewController.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "proprio", ofType: "mp3")!))
+            audioPlayer.prepareToPlay()
+            
+            let session = AVAudioSession.sharedInstance()
+            
+            do{
+                try session.setCategory(AVAudioSessionCategoryPlayback)
+            }catch{
+                print(error)
+            }
+            
+        }catch{
+            print(error)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func play(_ sender: Any) {
+        audioPlayer.play()
+    }
 
+    @IBAction func pause(_ sender: Any) {
+        if audioPlayer.isPlaying {
+            audioPlayer.pause()
+        }
+    }
+    @IBAction func restart(_ sender: Any) {
+        audioPlayer.currentTime = 0
+        audioPlayer.play()
+    }
+    @IBAction func stop(_ sender: Any) {
+        audioPlayer.currentTime = 0
+        audioPlayer.stop()
+    }
 }
 
