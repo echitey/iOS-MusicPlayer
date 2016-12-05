4756b665aac847e01f58aae8cd0c74db08597827
Yawo Echitey <Yawo@yawomac.local>
2016-12-05 15:01:48 -0200

    Added Slider for music Control

--- Music Player/ViewController.swift
+++ Music Player/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var Slider: UISlider!
 
     var audioPlayer = AVAudioPlayer()
+    var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,8 @@ class ViewController: UIViewController {
         do{
             audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "proprio", ofType: "mp3")!))
             audioPlayer.prepareToPlay()
+            
+            Slider.maximumValue = Float(audioPlayer.duration)
             
             let session = AVAudioSession.sharedInstance()
             
@@ -41,20 +45,38 @@ class ViewController: UIViewController {
 
     @IBAction func play(_ sender: Any) {
         audioPlayer.play()
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateSlider), userInfo: nil, repeats: true)
+        
     }
 
     @IBAction func pause(_ sender: Any) {
         if audioPlayer.isPlaying {
             audioPlayer.pause()
+            timer.invalidate()
         }
     }
     @IBAction func restart(_ sender: Any) {
+        timer.invalidate()
         audioPlayer.currentTime = 0
+        Slider.value = 0
         audioPlayer.play()
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.updateSlider), userInfo: nil, repeats: true)
     }
     @IBAction func stop(_ sender: Any) {
         audioPlayer.currentTime = 0
         audioPlayer.stop()
+        Slider.value = 0
+        timer.invalidate()
     }
+    
+    @IBAction func sliderChange(_ sender: Any) {
+        audioPlayer.currentTime = TimeInterval(Slider.value)
+    }
+    
+    func updateSlider(){
+        Slider.value = Float(audioPlayer.currentTime)
+    }
+    
 }
 
